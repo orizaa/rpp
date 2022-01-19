86ea369a8e596681b2b6977875cab14622ae6617
orizaa <97997854+orizaa@users.noreply.github.com>
2022-01-19 08:12:49 +0700

    Add files via upload

---
+++ rpp1.js
@@ -0,0 +1,9 @@
+let luas = 615
+let harga = 1500000
+let ppn = 15/100
+
+let total = luas * hargalet 
+let totalAkhir = total * ppn
+
+console.log('total = ' + total);
+console.log('totalAkhir = ' + totalAkhir);
